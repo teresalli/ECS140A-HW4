4b6158f6ace45fd45f856905e49247dea367d77c
teresalli <sqtli@ucdavis.edu>
2018-05-22 17:32:03 -0700

    update

--- hw4.pl
+++ hw4.pl
@@ -1,3 +1,136 @@
-/*******************************************/
-/**    Your solution goes in this file    **/ 
-/*******************************************/
+year_1953_1996_novels(Name) :-
+  novel(Name, Year),
+  (Year = 1953; Year = 1996).
+
+period_1800_1900_novels(Name) :-
+  novel(Name, Year),
+  Year >= 1800,
+  Year =< 1900.
+
+lotr_fans(Name) :-
+  fan(Name, List),
+  isMember(the_lord_of_the_rings, List).
+
+author_names(Name) :-
+  fan(chandler, List),
+  author(Name, Lists),
+  any_member(List, Lists).
+
+any_member([F|_], [F|_]) :- !.
+any_member(F1, [_|I2]) :-
+  any_member(F1, I2), !.
+any_member([_|I1], F2) :-
+  any_member(I1, F2), !.
+
+fans_names(Name) :-
+  author(brandon_sanderson, Lists),
+  fan(Name, List),
+  any_member(List, Lists).
+
+mutual_novels(Name) :-
+  fan(ross, List1),
+  fan(monica, List2),
+  mutual_check(Name, List1, List2).
+
+mutual_novels(Name) :-
+  fan(monica, List1),
+  fan(phoebe, List2),
+  mutual_check(Name, List1, List2).
+
+mutual_novels(Name) :-
+  fan(phoebe, List1),
+  fan(ross, List2),
+  mutual_check(Name, List1, List2).
+
+mutual_check(Name, [F|_], L2) :-
+  Name = F,
+  isMember(F, L2).
+
+mutual_check(Name, [_|R], L2) :-
+  mutual_check(Name, R, L2).
+
+isMember(Item, [Item|_]).
+isMember(Item, [_|R]) :-
+  isMember(Item, R).
+
+isUnion([], U, U).
+isUnion([F|R], Y, U) :-
+  isMember(F, Y),!,
+  isUnion(R, Y, U).
+isUnion([F|R], Y, [F|U]) :-
+  isUnion(R, Y, U).
+
+isIntersection([], _, []).
+isIntersection([F|R1], Y, I) :-
+  isMember(F, Y),!,
+  I = [F|R3],
+  isIntersection(R1, Y, R3).
+isIntersection([_|R1], Y, I) :-
+  isIntersection(R1, Y, I).
+
+isEqual(X, Y) :-
+  isIntersection(X, Y, Z),
+  Z = X.
+
+powerSet(X, Y) :-
+  bagof(Z, powerSetHelper(X, Z), H),
+  reverse(H, Y).
+
+powerSetHelper([], []).
+powerSetHelper([_|T], P) :-
+  powerSetHelper(T, P).
+powerSetHelper([H|T], [H|P]) :-
+  powerSetHelper(T, P).
+
+state(F, W, G, C).
+
+opposite(left, right).
+opposite(right, left).
+
+safe(state(F, _, G, _)) :-
+  F = G.
+safe(state(_, W, G, C)) :-
+  opposite(W, G),
+  opposite(G, C).
+
+unsafe(A) :- \+ safe(A).
+
+take(X,A,B) :-
+  opposite(A, B).
+
+arc(take(wolf, W1, W2), state(W1, W1, G, C), state(W2, W2, G, C)) :-
+  take(wolf, W1, W2),
+  safe(state(W2, W2, G, C)).
+
+arc(take(goat, G1, G2), state(G1, W, G1, C), state(G2, W, G2, C)) :-
+  take(goat, G1, G2),
+  safe(state(G2, W, G2, C)).
+
+arc(take(cabbage, C1, C2), state(C1, W, G, C1), state(C2, W, G, C2)) :-
+  take(cabbage, C1, C2),
+  safe(state(C2, W, G, C2)).
+
+arc(take(none, F1, F2), state(F1, W, G, C), state(F2, W, G, C)) :-
+  take(none, F1, F2),
+  safe(state(F2, W, G, C)).
+
+step(A, B, History) :-
+	safe(A),
+  S = arc(_, A, B),
+  S,
+  \+ isMember(S, History),
+  print_step([S|History]),
+	!.
+step(A, B, History) :-
+	safe(A),
+	S = arc(_, A, I),
+	\+ member(S, History),
+	S,
+	step(I, B, [S|History]).
+
+print_step([]).
+print_step([arc(take(X, A, B), _, _)|T]) :-
+	print_step(T),
+	print('take('), print(X), print(','), print(A), print(','), print(B), print(')'), nl.
+
+solve(F1, W1, G1, C1, F2, W2, G2, C2) :- step(state(F1, W1, G1, C1), state(F2, W2, G2, C2), []), !.
--- test.pl
+++ test.pl
@@ -1,8 +1,8 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% You can run all tests using "ty." 
+% You can run all tests using "ty."
 % Or you can run tests for individual parts, e.g., "test_isMember"
 %
-% Make sure that you have ``consulted'' all files you need to run 
+% Make sure that you have ``consulted'' all files you need to run
 % a particular test or tests. Remember to consult this file too.
 %
 % Use this to test everything and create Output.your, then exit from gprolog.
@@ -40,27 +40,27 @@ test :-
 
 test_year_1953_1996_novels :-
 	nl, printstring("TESTING: Novels published during the year 1953 or 1996"), nl, nl,
-	zeroInputOneOutput( year_1953_1996_novels, _, "1953_1996_Novel").
+	zeroInputOneOutputBag( year_1953_1996_novels, _, "1953_1996_Novel").
 
 test_period_1800_1900_novels :-
 	nl, printstring("TESTING: Novels published during the period 1800 to 1900"), nl, nl,
-	zeroInputOneOutput(  period_1800_1900_novels, _, "1800_1900_Novel").
+	zeroInputOneOutputBag(  period_1800_1900_novels, _, "1800_1900_Novel").
 
 test_lotr_fans :-
 	nl, printstring("TESTING: Characters who are fans of LOTR"), nl, nl,
-	zeroInputOneOutput(  lotr_fans, _, "LOTR_Fan").
+	zeroInputOneOutputBag(  lotr_fans, _, "LOTR_Fan").
 
 test_author_names :-
 	nl, printstring("TESTING: Authors of the novels owned by Chandler"), nl, nl,
-	zeroInputOneOutput( author_names, _, "Chandler's_novel_Author").
+	zeroInputOneOutputBag( author_names, _, "Chandler's_novel_Author").
 
 test_fans_names :-
 	nl, printstring("TESTING: Characters who are fans of Brandon Sanderson's novels"), nl, nl,
-	zeroInputOneOutput( fans_names, _, "Brandon_Sanderson_Fan" ).
+	zeroInputOneOutputBag( fans_names, _, "Brandon_Sanderson_Fan" ).
 
 test_mutual_novels :-
 	nl, printstring("TESTING:  Novels common between either of Phoebe, Ross, and Monica"), nl, nl,
-	zeroInputOneOutput( mutual_novels, _, "Phoebe_Ross_Monica_Novel").
+	zeroInputOneOutputSet( mutual_novels, _, "Phoebe_Ross_Monica_Novel").
 
 %-------------------------------------------- PART 2 --------------------------------------------
 
@@ -75,29 +75,29 @@ test_isMember :-
 
 test_isUnion :-
 	nl, printstring("TESTING: isUnion"), nl, nl,
-	twoInputOneOutput( isUnion, [], [], G, "XuY"),
-	twoInputOneOutput( isUnion, [], [a,b], G, "XuY"),
-	twoInputOneOutput( isUnion, [a], [a,b], G, "XuY"),
-	twoInputOneOutput( isUnion, [a,b], [a], G, "XuY"),
-	twoInputOneOutput( isUnion, [a,b], [b,c], G, "XuY"),
-	twoInputOneOutput( isUnion, [b,c], [a,b], G, "XuY"),
-	twoInputOneOutput( isUnion, [a,b], [c,d], G, "XuY"),
-	twoInputOneOutput( isUnion, [a,[b]], [a,[a,b]], G, "XuY"),
-	twoInputOneOutput( isUnion, [a,b,c], [a,[b,c]], G, "XuY"),
-	twoInputOneOutput( isUnion, [a,b,c], [a,[b,[d]],c], G, "XuY").
+	twoInputOneOutputSorted( isUnion, [], [], G, "XuY"),
+	twoInputOneOutputSorted( isUnion, [], [a,b], G, "XuY"),
+	twoInputOneOutputSorted( isUnion, [a], [a,b], G, "XuY"),
+	twoInputOneOutputSorted( isUnion, [a,b], [a], G, "XuY"),
+	twoInputOneOutputSorted( isUnion, [a,b], [b,c], G, "XuY"),
+	twoInputOneOutputSorted( isUnion, [b,c], [a,b], G, "XuY"),
+	twoInputOneOutputSorted( isUnion, [a,b], [c,d], G, "XuY"),
+	twoInputOneOutputSorted( isUnion, [a,[b]], [a,[a,b]], G, "XuY"),
+	twoInputOneOutputSorted( isUnion, [a,b,c], [a,[b,c]], G, "XuY"),
+	twoInputOneOutputSorted( isUnion, [a,b,c], [a,[b,[d]],c], G, "XuY").
 
 test_isIntersection :-
 	nl, printstring("TESTING: isIntersection"), nl, nl,
-	twoInputOneOutput( isIntersection, [], [], H, "XnY"),
-	twoInputOneOutput( isIntersection, [], [a,b], H, "XnY"),
-	twoInputOneOutput( isIntersection, [a], [a,b], H, "XnY"),
-	twoInputOneOutput( isIntersection, [a,b], [a], H, "XnY"),
-	twoInputOneOutput( isIntersection, [a,b], [b,c], H, "XnY"),
-	twoInputOneOutput( isIntersection, [b,c], [a,b], H, "XnY"),
-	twoInputOneOutput( isIntersection, [a,b], [c,d], H, "XnY"),
-	twoInputOneOutput( isIntersection, [a,[b]], [a,[a,b]], H, "XnY"),
-	twoInputOneOutput( isIntersection, [a,b,c], [a,[b,c]], H, "XnY"),
-	twoInputOneOutput( isIntersection, [a,b,c,[d]], [a,[b,[d]],c], H, "XnY").
+	twoInputOneOutputSorted( isIntersection, [], [], H, "XnY"),
+	twoInputOneOutputSorted( isIntersection, [], [a,b], H, "XnY"),
+	twoInputOneOutputSorted( isIntersection, [a], [a,b], H, "XnY"),
+	twoInputOneOutputSorted( isIntersection, [a,b], [a], H, "XnY"),
+	twoInputOneOutputSorted( isIntersection, [a,b], [b,c], H, "XnY"),
+	twoInputOneOutputSorted( isIntersection, [b,c], [a,b], H, "XnY"),
+	twoInputOneOutputSorted( isIntersection, [a,b], [c,d], H, "XnY"),
+	twoInputOneOutputSorted( isIntersection, [a,[b]], [a,[a,b]], H, "XnY"),
+	twoInputOneOutputSorted( isIntersection, [a,b,c], [a,[b,c]], H, "XnY"),
+	twoInputOneOutputSorted( isIntersection, [a,b,c,[d]], [a,[b,[d]],c], H, "XnY").
 
 test_isEqual :-
 	nl, printstring("TESTING: isEqual"), nl, nl,
@@ -112,32 +112,32 @@ test_isEqual :-
 	twoInputBoolOutput( isEqual, [a,[b],[c,d]], [a,[b],[d,c]]),
 	twoInputBoolOutput( isEqual, [a,[b,[c,d]]], [a,[[c,d],b]]).
 
-test_powerSet :- 
+test_powerSet :-
 	nl, printstring("TESTING: powerSet"), nl, nl,
-	oneInputOneOutput( powerSet, [], I, "P(X)"),
-	oneInputOneOutput( powerSet, [a], I, "P(X)"),
-	oneInputOneOutput( powerSet, [a,b], I, "P(X)"),
-	oneInputOneOutput( powerSet, [b,a], I, "P(X)"),
-	oneInputOneOutput( powerSet, [a,[b]], I, "P(X)"),
-	oneInputOneOutput( powerSet, [[a,b]], I, "P(X)"),
-	oneInputOneOutput( powerSet, [a,[]], I, "P(X)"),
-	oneInputOneOutput( powerSet, [a,b,[c]], I, "P(X)"),
-	oneInputOneOutput( powerSet, [a,[b,c]], I, "P(X)"),
-	oneInputOneOutput( powerSet, [a,[b,[c,[d]]]], I, "P(X)").
+	oneInputOneOutputSorted( powerSet, [], I, "P(X)"),
+	oneInputOneOutputSorted( powerSet, [a], I, "P(X)"),
+	oneInputOneOutputSorted( powerSet, [a,b], I, "P(X)"),
+	oneInputOneOutputSorted( powerSet, [b,a], I, "P(X)"),
+	oneInputOneOutputSorted( powerSet, [a,[b]], I, "P(X)"),
+	oneInputOneOutputSorted( powerSet, [[a,b]], I, "P(X)"),
+	oneInputOneOutputSorted( powerSet, [a,[]], I, "P(X)"),
+	oneInputOneOutputSorted( powerSet, [a,b,[c]], I, "P(X)"),
+	oneInputOneOutputSorted( powerSet, [a,[b,c]], I, "P(X)"),
+	oneInputOneOutputSorted( powerSet, [a,[b,[c,[d]]]], I, "P(X)").
 
 %-------------------------------------------- PART 3 --------------------------------------------
 
 test_puzzle :-  nl, printstring("TESTING puzzle"), nl, nl,
         solve(left,left,left,left,right,right,right,right), nl, nl,
-        fail; true. 
+        fail; true.
 
 test_puzzle2 :-  nl, printstring("TESTING puzzle"), nl, nl,
         solve(left,right,right,left,right,right,right,right), nl, nl,
-        fail; true. 
+        fail; true.
 
 test_puzzle3 :-  nl, printstring("TESTING puzzle"), nl, nl,
         solve(left,left,left,left,right,right,left,left), nl, nl,
-        fail; true. 
+        fail; true.
 
 
 %-------------------------------------------- TESTERS --------------------------------------------
@@ -145,6 +145,18 @@ test_puzzle3 :-  nl, printstring("TESTING puzzle"), nl, nl,
  * run Predicate on A1, A2, A3
  * assume output goes to A3, whose name is in the string SA3.
  */
+
+twoInputOneOutputSorted(Predicate,A1,A2,A3,SA3) :-
+	printstring("testing: "), write(Predicate),
+	printstring("("), write(A1),
+	printstring(","), write(A2),
+	printstring(","), printstring(SA3),
+	printstring(")"), nl,
+	T =..[Predicate,A1,A2,A3Unsort], call(T),
+	msort(A3Unsort, A3),
+	printstring(SA3), printstring(" (sorted) = "),
+	write(A3), nl, nl,
+	fail; true.
 
 twoInputOneOutput(Predicate,A1,A2,A3,SA3) :-
 	printstring("testing: "), write(Predicate),
@@ -154,13 +166,27 @@ twoInputOneOutput(Predicate,A1,A2,A3,SA3) :-
 	printstring(")"), nl,
 	T =..[Predicate,A1,A2,A3], call(T),
 	printstring(SA3), printstring(" = "),
-	write(A3), nl, nl,
+	printarr(A3), nl, nl,
 	fail; true.
 
 /* semi-general tester.
  * run Predicate on A1, A2
  * assume output goes to A2, whose name is in the string SA2.
  */
+
+
+
+oneInputOneOutputSorted(Predicate,A1,A2,SA2) :-
+	printstring("testing: "), write(Predicate),
+	printstring("("), write(A1),
+	%printstring(","), write(A2),
+	printstring(","), printstring(SA2),
+	printstring(")"), nl,
+	T =..[Predicate,A1,A2Unsort], call(T),
+	msort(A2Unsort, A2),
+	printstring(SA2), printstring(" (sorted) = "),
+	write(A2), nl, nl,
+	fail; true.
 
 oneInputOneOutput(Predicate,A1,A2,SA2) :-
 	printstring("testing: "), write(Predicate),
@@ -173,7 +199,7 @@ oneInputOneOutput(Predicate,A1,A2,SA2) :-
 	write(A2), nl, nl,
 	fail; true.
 
-/* semi-general tester.	
+/* semi-general tester.
  * run Predicate on A1,A2.
  * returns yes/no.
  */
@@ -188,10 +214,29 @@ twoInputBoolOutput(Predicate,A1,A2) :-
 	nl, nl,
 	fail; true.
 
-/* semi-general tester.	
+/* semi-general tester.
  * run Predicate on A1.
  * assume output goes to A1, whose name is in the string SA1.
  */
+
+zeroInputOneOutputBag(Predicate,A1,SA1) :-
+	printstring("testing: "), write(Predicate),
+	printstring("("), printstring(SA1),
+	printstring(")"), nl,
+	T =.. [Predicate,A1], bagof(A1, T, AsUnsort),
+	msort(AsUnsort, As),
+	printstring(SA1), printstring(" (sorted) = {"), nl,
+	printarr(As), printstring("}"), nl, nl,
+	fail; true.
+
+zeroInputOneOutputSet(Predicate,A1,SA1) :-
+	printstring("testing: "), write(Predicate),
+	printstring("("), printstring(SA1),
+	printstring(")"), nl,
+	T =.. [Predicate,A1], setof(A1, T, As),
+	printstring(SA1), printstring(" (sorted, deduplicated) = {"), nl,
+	printarr(As), printstring("}"), nl, nl,
+	fail; true.
 
 zeroInputOneOutput(Predicate,A1,SA1) :-
 	printstring("testing: "), write(Predicate),
@@ -207,3 +252,7 @@ zeroInputOneOutput(Predicate,A1,SA1) :-
 printstring([]).
 printstring([H|T]) :- put(H), printstring(T).
 
+printarr([]).
+printarr([A|As]) :-
+  printstring("  "), write(A), printstring(","), nl,
+  printarr(As).
